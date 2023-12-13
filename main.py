93251fc3ad1030dac04b3ae59285eb4dd6c9fa47
FatBat2001 <adhamekhalede@gmail.com>
2023-12-13 03:43:17 +0200

    did until req no 4

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import math
 from nltk import PorterStemmer
 
 ps = PorterStemmer()
+NO_OF_DOCUMENTS = 10
+
 def read_file(file_name):
     line = ""
     with open(file_name, "r") as file:
@@ -9,11 +12,10 @@ def read_file(file_name):
     return line
 
 
-
 def read_files():
     all_docs = {}
-    for i in range(1,11): # tokenization
-        all_docs[f'doc-{i}'] = list(filter(None,read_file(os.path.join("files",f"{i}.txt")).split(' ')))
+    for i in range(1, 11):  # tokenization
+        all_docs[f'doc-{i}'] = list(filter(None, read_file(os.path.join("files", f"{i}.txt")).split(' ')))
     return all_docs
 
 
@@ -38,13 +40,56 @@ def preprocess():
     # for key, postings in sorted_positional_index.items():
     #     print(f"{key} {postings}")
     return sorted_positional_index
+
+
 def compute_term_frequency(positional_index):
-        term_frequency = {key: [0 for i in range(0, 10)] for key in positional_index}
-        for term, posting_list in positional_index.items():
-            for doc in posting_list.keys():
-                term_frequency[term][int(doc.split('-')[1])-1] += len(posting_list[doc])
-        return term_frequency
+    term_frequency = {key: [0 for i in range(0, 10)] for key in positional_index}
+    for term, posting_list in positional_index.items():
+        for doc in posting_list.keys():
+            term_frequency[term][int(doc.split('-')[1]) - 1] += len(posting_list[doc])
+    return term_frequency
+
+
+def weight_term_mapping(n):
+    if n > 0:
+        return 1 + math.log10(n)
+    else:
+        return 0
+
+
+def compute_weighted_term_frequency(term_freq_table):
+    result = {}
+    for key, frequencies in term_freq_table.items():
+        result[key] = list(map(lambda x: weight_term_mapping(x), frequencies))
+    return result
+
+
+def calc_inverse_document_frequency(doc_freq):
+    return math.log10(NO_OF_DOCUMENTS / doc_freq)
+
+
+def compute_idf_weight(positional_index):
+    document_frequency = {key: [0, 0] for key in positional_index}
+    for key in positional_index.keys():
+        document_frequency[key][0] = len(positional_index[key])
+        document_frequency[key][1] = calc_inverse_document_frequency(document_frequency[key][0])
+    return document_frequency
+
+
+def compute_tf_idf_weight(df_table, tf_table):
+    tf_idf_weight = {}
+    for key in df_table:
+        tf_idf_weight[key] = list(map(lambda x: x * df_table[key][1], tf_table[key]))
+    print(tf_table)
+    return tf_idf_weight
+
 
 pos_index = preprocess()
 tf_table = compute_term_frequency(pos_index)
-print(tf_table)
+tf2 = compute_weighted_term_frequency(tf_table)
+
+# print(tf2)
+doc_freq = compute_idf_weight(pos_index)
+# print(doc_freq)
+compute_tf_idf_weight(doc_freq, tf2)
+

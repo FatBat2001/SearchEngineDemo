92a2eb40c61594389eff049fc54870d3d2ebde02
FatBat2001 <adhamekhalede@gmail.com>
2023-12-13 15:44:13 +0200

    intersecting threw multiple terms works :)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from nltk import PorterStemmer
 ps = PorterStemmer()
 NO_OF_DOCUMENTS = 10
 
+
 def read_file(file_name):
     line = ""
     with open(file_name, "r") as file:
@@ -19,7 +20,13 @@ def read_files():
     return all_docs
 
 
-def preprocess():
+def preprocess(text):
+    tokenized_text = list(filter(None, text.split(' ')))
+    stemmed_text = list(map(lambda token: ps.stem(token), tokenized_text))
+    return stemmed_text
+
+
+def init():
     all_documents = read_files()
     positional_index = {}
     for doc_name, content in all_documents.items():
@@ -40,6 +47,7 @@ def preprocess():
     # for key, postings in sorted_positional_index.items():
     #     print(f"{key} {postings}")
     return sorted_positional_index
+
 
 
 def compute_term_frequency(positional_index):
@@ -83,13 +91,48 @@ def compute_tf_idf_weight(df_table, tf_table):
     print(tf_table)
     return tf_idf_weight
 
+def get_posting_list(word):
+    if word not in pos_index:
+        print('key not present')
+        return []
+    result = list(pos_index[word].keys())
+    return result
 
-pos_index = preprocess()
-tf_table = compute_term_frequency(pos_index)
-tf2 = compute_weighted_term_frequency(tf_table)
 
-# print(tf2)
-doc_freq = compute_idf_weight(pos_index)
-# print(doc_freq)
-compute_tf_idf_weight(doc_freq, tf2)
+# can be optimized using binary_search later
+def intersect(list1, list2):
+    p1 = 0
+    p2 = 0
+    result = []
+    while p1 < len(list1) and p2 < len(list2):
+        if list1[p1] == list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+            p2 += 1
+        elif list1[p1] < list2[p2]:
+            p1 += 1
+        else:
+            p2 += 1
+    return result
+
+
+def get_phrase_query(query):
+    preprocessed_query = preprocess(query)
+    result = get_posting_list(preprocessed_query[0])
+    for i in range(1, len(preprocessed_query)):
+        result = intersect(result, get_posting_list(preprocessed_query[i]))
+    return result
+
+
+pos_index = init()
+# print(pos_index)
+input_query = "flies adham"
+res = get_phrase_query(input_query)
+print(res)
+# tf = compute_term_frequency(pos_index)
+# w_tf = compute_weighted_term_frequency(tf)
+# # print(tf2)
+# idf = compute_idf_weight(pos_index)
+# # print(doc_freq)
+# tf_idf_w = compute_tf_idf_weight(idf, w_tf)
 
